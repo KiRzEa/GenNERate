a279d9ee664be5b7fb86103167758b0ad377c528
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-06-03 08:52:09 +0000

    debug

--- scripts/prompt.py
+++ scripts/prompt.py
@@ -1,26 +1,49 @@
+# PROMPT = """\
+# ##System Prompt
+# This is a Named Entity Recognition (NER) task for Vietnamese text.
+# Given a piece of Vietnamese text, identify and classify the named entities (NE) into three categories: locations, organizations, and persons with these available labels: 'ORGANIZATION_inner', \
+# 'PERSON_outer', \
+# 'ORGANIZATION_outer', \
+# 'LOCATION_outer', \
+# 'MISCELLANEOUS_inner', \
+# 'LOCATION_inner', \
+# 'PERSON_inner', \
+# 'MISCELLANEOUS_outer' \
+# If no named entities found, return None
+# ##Example
+# Input sentence: Một sĩ quan cấp cao vừa bị tạm giữ để thẩm vấn về cáo buộc giúp cựu Thủ tướng Thái Lan Yingluck Shinawatra chạy khỏi đất nước.
+# Entities with label:
+# Thái Lan::LOCATION_outer
+# Yingluck::PERSON_outer
+
+# Input sentence: Nỗi đau của người bệnh khi bị tiêm, kéo dài vài giây, vài phút, cùng lắm có thể đến vài ngày.
+# Entities with label:
+# None
+
+# ##Model Response
+# Input sentence: {}
+# Entities with label:
+# """
+
 PROMPT = """\
-##System Prompt
-This is a Named Entity Recognition (NER) task for Vietnamese text.
-Given a piece of Vietnamese text, identify and classify the named entities (NE) into three categories: locations, organizations, and persons with these available labels: 'ORGANIZATION_inner', \
-'PERSON_outer', \
-'ORGANIZATION_outer', \
-'LOCATION_outer', \
-'MISCELLANEOUS_inner', \
-'LOCATION_inner', \
-'PERSON_inner', \
-'MISCELLANEOUS_outer' \
-If no named entities found, return None
-##Example
-Input sentence: Một sĩ quan cấp cao vừa bị tạm giữ để thẩm vấn về cáo buộc giúp cựu Thủ tướng Thái Lan Yingluck Shinawatra chạy khỏi đất nước.
-Entities with label:
-Thái Lan::LOCATION_outer
-Yingluck::PERSON_outer
+##SystemPrompt:
+You are a Vietnamese COVID-19 named entity recognition expert. \
+You are trained to identify people, organizations, locations, dates, and other relevant entities according to the following categories and definitions:
 
-Input sentence: Nỗi đau của người bệnh khi bị tiêm, kéo dài vài giây, vài phút, cùng lắm có thể đến vài ngày.
-Entities with label:
-None
+* PATIENT_ID: Unique identifier of a COVID-19 patient in Vietnam.
+* PERSON_NAME: Name of a patient or person who comes into contact with a patient.
+* AGE: Age of a patient or person who comes into contact with a patient.
+* GENDER: Gender of a patient or person who comes into contact with a patient.
+* OCCUPATION: Job of a patient or person who comes into contact with a patient.
+* LOCATION: Locations/places that a patient was presented at.
+* ORGANIZATION: Organizations related to a patient, e.g., company, government organization.
+* SYMPTOM & DISEASE: Symptoms that a patient experiences and diseases a patient had prior to COVID-19 or complications.
+* TRANSPORTATION: Means of transportation that a patient used, including flight numbers and bus/car plates.
+* DATE: Any date that appears in the sentence.
 
-##Model Response
-Input sentence: {}
-Entities with label:
-"""
+List of named entities found in the sentence, categorized according to the definitions above. \
+Each entity should be tagged with its corresponding category (e.g., PERSON_NAME: Nguyen Van Nam, LOCATION: Ha Noi). \
+If no named entities found, return None.
+
+**Input sentence**: {}
+**Output:**"""
--- scripts/utils.py
+++ scripts/utils.py
@@ -23,10 +23,27 @@ def construct_prompt(df):
     assert len(input_text) == len(output_text)
     return pd.DataFrame(list(zip(input_text, output_text)), columns=['text', 'label'])
 
+def extract_aspect(tokens, ner_tags):
+    assert len(tokens) == len(ner_tags)
+    entities = []
+    entity_tokens = []
+    for idx, (token, tag) in enumerate(zip(tokens, ner_tags)):
+        if tag.startswith('B'):
+          entity_tokens = [token]
+          entity_type = tag[2:]
+        elif tag.startswith('I'):
+          entity_tokens.append(token)
+        elif tag == 'O':
+          if len(entity_tokens) > 0:
+            entities.append(' '.join(entity_tokens)+f'::{entity_type}')
+            entity_tokens = []
+
+    return entities
+
 def create_dataset(data_dir):
-    train = read_data(f'{data_dir}/train_sentence.json')
-    dev = read_data(f'{data_dir}/dev_sentence.json')
-    test = read_data(f'{data_dir}/test_sentence.json')
+    train = read_data(f'{data_dir}/train_word.json')
+    dev = read_data(f'{data_dir}/dev_word.json')
+    test = read_data(f'{data_dir}/test_word.json')
     dataset = DatasetDict({
         'train': Dataset.from_pandas(construct_prompt(train)),
         'dev': Dataset.from_pandas(construct_prompt(dev)),
@@ -48,6 +65,10 @@ def print_trainable_parameters(model):
     print(
         f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
     )
+
+
+def get_labels():
+    return ['AGE', 'DATE', 'GENDER', 'JOB', 'LOCATION', 'NAME', 'ORGANIZATION', 'PATIENT_ID', 'SYMPTOM_AND_DISEASE', 'TRANSPORTATION']
 
 def evaluate(preds, golds, output_file="results.txt"):
     tp = 0.
