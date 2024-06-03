7eb8e736202d179904dbe3119a458a0b0b104486
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-06-03 05:31:57 +0000

    debug

--- scripts/main.py
+++ scripts/main.py
@@ -16,7 +16,7 @@ parser.add_argument("--model_name", type=str, default="bigscience/bloom-560m")
 parser.add_argument("--lr", type=float, default=2e-4)
 parser.add_argument("--num_epochs", type=int, default=10)
 parser.add_argument("--batch_size", type=int, default=16)
-parser.add_argument("--data_dir", type=str, default"../data/sentence")
+parser.add_argument("--data_dir", type=str, default="../data/sentence")
 
 args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
