b6d574f1b53ab5f859f70e74b04b98b038ee63ef
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-06-04 16:55:33 +0000

    debug

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 !python main.py model_name bigscience/bloom-560m \
                 --num_epochs 1 \
                 --batch_size 1 \
-                --data_dir ''
+                --data_dir '' \
+                --syllable
