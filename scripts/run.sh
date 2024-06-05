b9486cc194d637de3c3ff7bc91197e91b5e55a7b
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-06-05 13:02:48 +0000

    debug

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
-!python main.py model_name bigscience/bloom-560m \
-                --num_epochs 1 \
-                --batch_size 1 \
-                --data_dir '' \
+!python main.py model_name bigscience/bloomz-7b1 \
+                --num_epochs 20 \
+                --batch_size 16 \
+                --data_dir '../data/phoner-covid19' \
                 --syllable
