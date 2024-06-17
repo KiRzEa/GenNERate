fd54d2540cf5e2b0554cb362c00e77f7d9167709
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-06-17 12:43:50 +0700

    debug

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -78,7 +78,8 @@ class NERTrainingPipeline:
             learning_rate=self.lr,
             report_to="all",
             bf16=self.bf16,
-            fp16=self.fp16
+            fp16=True,
+            half_precision_backend="auto"
         )
         
         self.dataset = self.create_dataset()
@@ -185,8 +186,8 @@ class NERTrainingPipeline:
             list: The list of predicted labels.
         """
         inputs = self.tokenizer(instruction_template + example + response_template, return_tensors="pt", max_length=512, truncation=True, padding="max_length")
-        input_ids = inputs.input_ids.to(self.device)
-        attention_mask = inputs.attention_mask.to(self.device)
+        input_ids = inputs.input_ids.to(self.device).half()
+        attention_mask = inputs.attention_mask.to(self.device).half()
         
         outputs = self.trainer.model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=128, eos_token_id=self.tokenizer.eos_token_id)
         
