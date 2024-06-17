e53d1b526789b8cfe85a2951d7480efbd09ec96a
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-06-17 22:00:20 +0700

    use Trainer

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -2,9 +2,9 @@ import os
 import time
 import torch
 from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, TrainingArguments, GenerationConfig, BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, TrainingArguments, Trainer, GenerationConfig, BitsAndBytesConfig, DataCollatorForLanguageModeling
 from peft import LoraConfig, get_peft_model, TaskType
-from trl import SFTTrainer, DataCollatorForCompletionOnlyLM, setup_chat_format
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 from tqdm import tqdm
 import pandas as pd
 
@@ -83,30 +83,33 @@ class NERTrainingPipeline:
         )
         
         self.dataset = self.create_dataset()
+        self.processed_datasets = self.preprocess_datasets()
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, token='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')
-        self.base_model = AutoModelForCausalLM.from_pretrained(self.model_name, 
-                                                                token='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',
-                                                                # quantization_config=self.quant_config,
-                                                                # torch_dtype=torch.float16,
-                                                                device_map="auto")
+        self.base_model = AutoModelForCausalLM.from_pretrained(self.model_name, device_map="auto")
+        self.model = get_peft_model(self.base_model, self.peft_config)
+        self.collator = DataCollatorForLanguageModeling(tokenizer=self.tokenizer, mlm=False)
 
-        response_template_ids = self.tokenizer.encode(response_template, add_special_tokens=False)[1:]
-        self.collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=self.tokenizer)
+        # response_template_ids = self.tokenizer.encode(response_template, add_special_tokens=False)[1:]
+        # self.collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=self.tokenizer)
+        # self.processed_datasets = self.dataset.remove_columns([col for col in self.dataset['train'].column_names if col not in ['input', 'output']])        
+        self.max_input_length, self.max_output_length, self.max_length = self.get_max_lengths()
 
-        
-        self.max_length = self.get_max_lengths()
-
-        self.processed_datasets = self.dataset.remove_columns([col for col in self.dataset['train'].column_names if col not in ['input', 'output']])
         print(self.processed_datasets)
 
-        self.trainer = SFTTrainer(
-            model=self.base_model,
-            train_dataset=self.processed_datasets["train"],
-            peft_config=self.peft_config,
-            max_seq_length=self.max_length,
+        # self.trainer = SFTTrainer(
+        #     model=self.base_model,
+        #     train_dataset=self.processed_datasets["train"],
+        #     peft_config=self.peft_config,
+        #     max_seq_length=self.max_length,
+        #     args=self.training_arguments,
+        #     data_collator=self.collator,
+        #     formatting_func=formatting_prompts_func
+        # )
+        self.trainer = Trainer(
+            model=self.model,
             args=self.training_arguments,
-            data_collator=self.collator,
-            formatting_func=formatting_prompts_func
+            train_dataset=self.processed_datasets["train"],
+            data_collator=data_collator
         )
 
         self.print_trainable_parameters()
@@ -130,6 +133,72 @@ class NERTrainingPipeline:
                             "down_proj",
                         ]
             return target_modules
+
+    def preprocess_function(self, examples):
+        """
+        Preprocess the dataset examples.
+
+        Args:
+            examples (dict): A dictionary containing input and output examples.
+
+        Returns:
+            dict: Preprocessed model inputs.
+        """
+        batch_size = len(examples["input"])
+        inputs = [instruction_template + item + response_template + " " for item in examples["input"]]
+        targets = examples["output"]
+        model_inputs = self.tokenizer(inputs)
+        labels = self.tokenizer(targets, add_special_tokens=False)
+
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i] + [self.tokenizer.eos_token_id]
+            model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
+            labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
+            model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
+
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i]
+            model_inputs["input_ids"][i] = [self.tokenizer.pad_token_id] * (self.max_length - len(sample_input_ids)) + sample_input_ids
+            model_inputs["attention_mask"][i] = [0] * (self.max_length - len(sample_input_ids)) + model_inputs["attention_mask"][i]
+            labels["input_ids"][i] = [-100] * (self.max_length - len(sample_input_ids)) + label_input_ids
+            model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:self.max_length])
+            model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:self.max_length]) 
+            labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:self.max_length])
+
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+
+    def preprocess_datasets(self):
+        """
+        Preprocess the training and testing datasets.
+
+        Returns:
+            DatasetDict: The preprocessed datasets.
+        """
+        return self.dataset.map(
+            self.preprocess_function,
+            batched=True,
+            num_proc=1,
+            remove_columns=self.dataset["train"].column_names,
+            load_from_cache_file=False,
+            desc="Running tokenizer on dataset",
+        )
+
+    def get_max_lengths(self):
+        """
+        Calculate the maximum lengths of input and output sequences.
+
+        Returns:
+            tuple: A tuple containing the maximum input length, maximum output length, and maximum combined length.
+        """
+        output_length = [len(self.tokenizer(review)["input_ids"]) for review in self.dataset['train']["output"]]
+        input_length = [len(self.tokenizer(instruction_template + review + response_template)["input_ids"]) for review in self.dataset['train']["input"]]
+        max_output_length = max(output_length)
+        max_input_length = max(input_length)
+        max_length = max([inp + out for inp, out in zip(output_length, input_length)])
+        return max_input_length, max_output_length, max_length
 
     def create_dataset(self):
         """
@@ -155,14 +224,22 @@ class NERTrainingPipeline:
             f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
         )
 
-    def get_max_lengths(self):
+    def get_prediction(self, examples):
         """
-        Calculate the maximum lengths of input and output sequences.
+        Generate predictions for input examples.
+
+        Args:
+            examples (str): The input examples.
 
         Returns:
-            tuple: A tuple containing the maximum input length, maximum output length, and maximum combined length.
+            list: The list of predicted labels.
         """
-        return max([len(self.tokenizer(PROMPT.format(inp, out)).input_ids) for inp, out in zip(self.dataset['train']['input'], self.dataset['train']['output'])])
+        input_ids = self.tokenizer([instruction_template + example + response_template for example in examples],
+         max_length=self.max_input_length, return_tensors="pt", padding="max_length", truncation=True
+         ).input_ids.to(self.device)
+        outputs = self.trainer.model.generate(input_ids=input_ids, max_new_tokens=self.max_output_length, eos_token_id=self.tokenizer.eos_token_id)
+        preds = outputs[:, self.max_input_length:].detach().cpu().numpy()
+        return self.tokenizer.batch_decode(preds, skip_special_tokens=True)
 
     def train(self):
         """
@@ -185,11 +262,11 @@ class NERTrainingPipeline:
         Returns:
             list: The list of predicted labels.
         """
-        inputs = self.tokenizer([instruction_template + example + response_template for example in examples], return_tensors="pt", max_length=512, truncation=True, padding="max_length")
+        inputs = self.tokenizer([instruction_template + example + response_template for example in examples], return_tensors="pt", max_length=self.max_input_length, truncation=True, padding="max_length")
         input_ids = inputs.input_ids.to(self.device)
         attention_mask = inputs.attention_mask.to(self.device)
         
-        outputs = self.trainer.model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=128, eos_token_id=self.tokenizer.eos_token_id)
+        outputs = self.trainer.model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=self.max_output_length, eos_token_id=self.tokenizer.eos_token_id)
         
         preds = outputs.detach().cpu().numpy()
         return self.tokenizer.batch_decode(preds, skip_special_tokens=True)
